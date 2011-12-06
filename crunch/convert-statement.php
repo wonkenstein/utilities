639e8bd9ea2adda458fd632f6ad31bd75ed1cb7f
Mark Wong <marky.wong@gmail.com>
2011-12-06 00:36:47 +0000

    converting tabs to spaces

--- crunch/convert-statement.php
+++ crunch/convert-statement.php
@@ -10,8 +10,8 @@
 $file = $argv[1];
 
 if (!file_exists($file)) {
-	echo $file . ' does not exist' . "\n";
-	return;
+  echo $file . ' does not exist' . "\n";
+  return;
 }
 
 $file_contents = file_get_contents($file);
@@ -25,35 +25,35 @@ unset($file_contents[count($file_contents)-1]);
 $results = array();
 foreach ($file_contents as $line) {
 
-	$line = explode(',', $line);
+  $line = explode(',', $line);
 
-	// don't want the first column as text we don't need it
-	array_shift($line);
+  // don't want the first column as text we don't need it
+  array_shift($line);
 
-	// change date to
-	// according to docs http://www.php.net/manual/en/function.strtotime.php
-	// "Dates in the m/d/y or d-m-y formats are disambiguated by looking at the
-	// separator between the various components: if the separator is a slash (/),
-	// then the American m/d/y is assumed; whereas if the separator is a dash (-)
-	// or a dot (.), then the European d-m-y format is assumed.
-	$date = str_replace('/', '-', $line[0]);
-	$date = date('Y-m-d', strtotime($date));
-	$results[$date] = $line;
+  // change date to
+  // according to docs http://www.php.net/manual/en/function.strtotime.php
+  // "Dates in the m/d/y or d-m-y formats are disambiguated by looking at the
+  // separator between the various components: if the separator is a slash (/),
+  // then the American m/d/y is assumed; whereas if the separator is a dash (-)
+  // or a dot (.), then the European d-m-y format is assumed.
+  $date = str_replace('/', '-', $line[0]);
+  $date = date('Y-m-d', strtotime($date));
+  $results[$date] = $line;
 }
 
 // sort on the date
 ksort($results);
 
 $headers = array(
-	'Date',
-	'Reference',
-	'Amount',
-	'Balance',
+  'Date',
+  'Reference',
+  'Amount',
+  'Balance',
 );
 
 // output as csv
 echo implode(',', $headers), "\n";
 
 foreach ($results as $k => $v) {
-	echo implode(',', $v), "\n";
+  echo implode(',', $v), "\n";
 }
