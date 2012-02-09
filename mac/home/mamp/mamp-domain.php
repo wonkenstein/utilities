4ea1184bc1b0b79febbb5db325f4f0f646f98109
Mark Wong <mark@wonkenstein.com>
2012-02-09 00:23:57 +0000

    Updating vhost creation script to be interactive and append to hosts and create vhosts files if required

--- mac/home/mamp/mamp-domain.php
+++ mac/home/mamp/mamp-domain.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * Run as sudo
+ * PHP script as running MAMP which has php, should be able to be ported to
+ * WAMP or XAMPP
  */
-
-
-$MAMP_ROOT = '/Applications/MAMP/';
-$DOC_ROOT = '/Users/markwong/workspace/';
+$MAMP_ROOT = '/Applications/MAMP';
+$DOC_ROOT = '/Users/markwong/workspace';
 $HOSTS_FILE = '/private/etc/hosts';
 $VHOSTS_TEMPLATE = 'vhost.tpl.txt';
-//$HOSTS_FILE = 'hosts';
+$USER = 'markwong';
+
 $settings = array();
 set_setting($settings, 'domain');
 set_setting($settings, 'doc_root');
@@ -16,19 +17,29 @@ set_setting($settings, 'email');
 set_setting($settings, 'parse_html');
 set_setting($settings, 'utf8');
 
-//var_dump($settings);
-//echo $line;
-// Add hosts file entry
-/*
-$host_entry = "127.0.0.1\t" . $settings['domain'];
-$res = file_put_contents($HOSTS_FILE, "$host_entry\n", FILE_APPEND);
 
-if (!$res) {
-  echo "Couldn't write to hosts file $HOSTS_FILE\n";
-  echo "Try running script using sudo\n";
-  exit;
+// Add hosts file entry
+$host_entry = "127.0.0.1\t" . $settings['domain'];
+
+$contents = file_get_contents($HOSTS_FILE);
+
+// check if exists in our hosts file
+if (strpos($contents, $host_entry) > -1) {
+  echo "Host entry exists in " . $HOSTS_FILE . "\n";
 }
-*/
+else {
+  $res = file_put_contents($HOSTS_FILE, "$host_entry\n", FILE_APPEND);
+
+  if (!$res) {
+    echo "Couldn't write to hosts file $HOSTS_FILE\n";
+    echo "Try running script using sudo\n";
+    exit;
+  }
+    else {
+      echo "Added $host_entry to $HOSTS_FILE\n";
+  }
+}
+
 
 
 // copy the vhosts
@@ -36,7 +47,7 @@ $vhost = file_get_contents($VHOSTS_TEMPLATE);
 
 $vhost = str_replace('[EMAIL]', $settings['email'], $vhost);
 $vhost = str_replace('[DOMAIN]', $settings['domain'], $vhost);
-$vhost = str_replace('[DOCUMENT_ROOT]', $settings['doc_root'], $vhost);
+$vhost = str_replace('[DOCUMENT_ROOT]', $DOC_ROOT . '/' .$settings['doc_root'], $vhost);
 
 if (strtolower($settings['parse_html']) == 'yes') {
   $vhost = str_replace(
@@ -53,10 +64,31 @@ if (strtolower($settings['utf8']) == 'yes') {
       );
 
 }
-echo $vhost;
+
 $filename = $settings['domain'] . '.conf';
-file_put_contents($MAMP_ROOT . '/conf/apache/extra/vhost/' . $filename, $vhost);
-file_put_contents($filename, $vhost);
+$filepath = $MAMP_ROOT . '/conf/apache/extra/vhost/' . $filename;
+
+if (file_exists($filepath)) {
+  echo "$filepath File Exists\n";
+  echo "Overwrite?";
+  $write = trim(fgets(STDIN));
+  $write = (strtolower($write) == 'yes') ? true : false;
+}
+else {
+  $write = true;
+}
+
+if ($write) {
+  $res = file_put_contents($filepath, $vhost);
+
+  if ($res) {
+    echo "Wrote conf file " . $filepath . "\n";
+    chown($filepath, $USER); // change owner to default user
+  }
+  else {
+    echo "Could not write conf file " . $filepath . "\n";
+  }
+}
 
 
 //
